0921e007b06b71b1430a9b5f8e9f98cd4f0e0a25
nate <70911747+nathanallen242@users.noreply.github.com>
2023-12-29 14:49:05 -0500

    added password change to authcontext

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,5 +1,5 @@
 import React, { createContext, useState, useEffect } from 'react';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updatePassword as updateFirebasePassword, EmailAuthProvider, reauthenticateWithCredential } from "firebase/auth";
 import { getDatabase, ref, set } from "firebase/database";
 import { FIREBASE_APP } from '../config/FireBase';
 import { registerIndieID, unregisterIndieDevice } from 'native-notify';
@@ -8,83 +8,108 @@ import { EXPO_APP_ID, EXPO_APP_TOKEN } from '@env';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
- const [user, setUser] = useState(null);
- const [error, setError] = useState(null);
- const [notificationsEnabled, setNotificationsEnabled] = useState(true);
- const auth = getAuth(FIREBASE_APP);
- const database = getDatabase(FIREBASE_APP);
+  const [user, setUser] = useState(null);
+  const [error, setError] = useState(null);
+  const [notificationsEnabled, setNotificationsEnabled] = useState(true);
+  const auth = getAuth(FIREBASE_APP);
+  const database = getDatabase(FIREBASE_APP);
 
- const updatePassword = async (password) => {
-    if (user) {
-      await updatePassword(user, password);
-    }
-  };
-
- useEffect(() => {
-   const unsubscribe = onAuthStateChanged(auth, (user) => {
-     if (user) {
-       setUser(user);
-     } else {
-       setUser(null);
-     }
-   });
-
-   return () => unsubscribe();
- }, []);
-
- return (
-   <AuthContext.Provider
-     value={{
-       user,
-       setUser,
-       updatePassword,
-       error,
-       setError,
-       notificationsEnabled,
-       setNotificationsEnabled,
-       login: async (email, password) => {
-         try {
-           const userCredential = await signInWithEmailAndPassword(auth, email, password);
-           const user = userCredential.user;
-           setUser(user);
-           setError(null);
-           // Native Notify Indie ID registration
-           registerIndieID(user.uid, EXPO_APP_ID, EXPO_APP_TOKEN);
-         } catch (e) {
-           setError(e.message);
-           console.error(e);
-         }
-       },
-       register: async (email, password) => {
-         try {
-           const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-           const user = userCredential.user;
-           await set(ref(database, 'users/' + user.uid), {
-             email: email,
-             uid: user.uid
+ const updatePassword = async (currentPassword, newPassword) => {
+   if (user) {
+     // Create a credential
+     const credential = EmailAuthProvider.credential(
+       user.email,
+       currentPassword
+     );
+ 
+     // Reauthenticate the user
+     return reauthenticateWithCredential(user, credential)
+       .then(() => {
+         // User reauthenticated
+         // Now we can update the password
+         return updateFirebasePassword(user, newPassword)
+           .then(() => {
+             setError(null);
+           })
+           .catch((error) => {
+             // An error occurred
+             setError(error.message);
+             throw error;
            });
-           setUser(user);
-           setError(null);
-           // Native Notify Indie ID registration
-           registerIndieID(user.uid, EXPO_APP_ID, EXPO_APP_TOKEN);
-         } catch (e) {
-           setError(e.message);
-           console.error(e);
-         }
-       },
-       logout: async () => {
-         try {
-           // Native Notify Indie Push Registration Code
-           unregisterIndieDevice(user.uid, EXPO_APP_ID, EXPO_APP_TOKEN);
-           await signOut(auth);
-           setUser(null);
-         } catch (e) {
-           console.error(e);
-         }
-       },
-     }}
-   >
-     {children}
-   </AuthContext.Provider>
- );
+       })
+       .catch((error) => {
+         // The reauthentication failed
+         setError('Current password is incorrect.');
+         throw error;
+       });
+   }
+ };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        setUser,
+        updatePassword,
+        error,
+        setError,
+        notificationsEnabled,
+        setNotificationsEnabled,
+        login: async (email, password) => {
+          try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            setUser(user);
+            setError(null);
+            // Native Notify Indie ID registration
+            registerIndieID(user.uid, EXPO_APP_ID, EXPO_APP_TOKEN);
+          } catch (e) {
+            setError(e.message);
+            console.error(e);
+          }
+        },
+        register: async (email, password) => {
+          try {
+            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+            const user = userCredential.user;
+            await set(ref(database, 'users/' + user.uid), {
+              email: email,
+              uid: user.uid
+            });
+            setUser(user);
+            setError(null);
+            // Native Notify Indie ID registration
+            registerIndieID(user.uid, EXPO_APP_ID, EXPO_APP_TOKEN);
+          } catch (e) {
+            setError(e.message);
+            console.error(e);
+          }
+        },
+        logout: async () => {
+          try {
+            // Native Notify Indie Push Registration Code
+            unregisterIndieDevice(user.uid, EXPO_APP_ID, EXPO_APP_TOKEN);
+            await signOut(auth);
+            setUser(null);
+          } catch (e) {
+            console.error(e);
+          }
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,15 +1,39 @@
 import React, { useState, useEffect, useContext } from 'react';
-import { View, StyleSheet, TouchableOpacity, Image, Alert } from 'react-native';
-import { Text, Button } from 'react-native-elements';
+import { View, StyleSheet, TouchableOpacity, Image, Alert, Modal, TouchableWithoutFeedback } from 'react-native';
+import { Text, Button, Switch, Icon } from 'react-native-elements';
 import { getDatabase, ref, set, onValue } from "firebase/database";
 import { FIREBASE_DB } from '../config/FireBase';
 import { AuthContext } from '../contexts/AuthContext';
 import * as ImagePicker from 'expo-image-picker';
+import { TextInput } from 'react-native-gesture-handler';
 
 const ProfileScreen = () => {
-  const { user } = useContext(AuthContext);
+  const { user, updatePassword, notificationsEnabled, setNotificationsEnabled } = useContext(AuthContext);
   const [image, setImage] = useState(null);
   const [email, setEmail] = useState(user ? user.email : '');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handlePasswordChange = async () => {
+    if (newPassword !== confirmPassword) {
+      Alert.alert('New passwords do not match!');
+      return;
+    }
+    // Call updatePassword from AuthContext
+    try {
+      await updatePassword(currentPassword, newPassword);
+      setNewPassword('');
+      setConfirmPassword('');
+      setCurrentPassword('');
+      setModalVisible(false);
+      Alert.alert('Password updated successfully!');
+    } catch (error) {
+      // An error occurred
+      Alert.alert(error.message);
+    }
+  };
 
   useEffect(() => {
     if (user) {
@@ -32,7 +56,7 @@ const ProfileScreen = () => {
       aspect: [4, 3],
       quality: 1,
     });
-  
+
     if (!result.canceled) {
       const uri = result.assets[0].uri;
       setImage(uri);
@@ -55,6 +79,61 @@ const ProfileScreen = () => {
         </TouchableOpacity>
       )}
       <Text style={styles.emailStyle}>{email}</Text>
+      <Button title="Change Password" onPress={() => setModalVisible(true)} />
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <View style={styles.closeButtonContainer}>
+              <TouchableWithoutFeedback onPress={() => setModalVisible(false)}>
+                <Icon name='close' type='material' color='#517fa4' />
+              </TouchableWithoutFeedback>
+            </View>
+            <Text h4 style={styles.modalText}>Submit New Password</Text>
+            <TextInput
+              style={styles.input}
+              value={currentPassword}
+              onChangeText={setCurrentPassword}
+              placeholder="Current Password"
+              placeholderTextColor="#808080"
+              secureTextEntry={true}
+            />
+            <TextInput
+              style={styles.input}
+              value={newPassword}
+              onChangeText={setNewPassword}
+              placeholder="New Password"
+              placeholderTextColor="#808080"
+              secureTextEntry={true}
+            />
+            <TextInput
+              style={styles.input}
+              value={confirmPassword}
+              onChangeText={setConfirmPassword}
+              placeholder="Confirm New Password"
+              placeholderTextColor="#808080"
+              secureTextEntry={true}
+            />
+            <Button title="Submit" onPress={handlePasswordChange} />
+          </View>
+        </View>
+      </Modal>
+      <View style={styles.switchContainer}>
+        <Text>Enable Push Notifications</Text>
+        <Switch
+          value={notificationsEnabled}
+          onValueChange={setNotificationsEnabled}
+          trackColor={{ false: '#767577', true: 'green' }}
+          thumbColor={notificationsEnabled ? 'white' : '#f4f3f4'}
+          ios_backgroundColor="#3e3e3e"
+        />
+      </View>
     </View>
   );
 };
@@ -82,6 +161,59 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginBottom: 30,
   },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '100%',
+    paddingHorizontal: 20,
+    paddingVertical: 30,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5
+  },
+  closeButtonContainer: {
+    position: 'absolute',
+    top: 10,
+    right: 10
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center",
+    fontWeight: 'bold',
+    fontSize: 20,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    width: 200,
+  }
 });
 
 export default ProfileScreen;
